e8ce1c257366b9438b15d3dd8acf280acee2b9ce
Ansgar <fehnker@gmail.com>
2020-11-15 11:14:20 +0100

    Solution plus small edit

--- exercises/bubble.py
+++ exercises/bubble.py
@@ -32,27 +32,25 @@ class Bubble:
         screen.blit(text, (self.x + 22, self.y + 12))
 
     def collide(self, other):
-        if not (self is other):
-            dist_now = math.sqrt(math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2))
-            dist_next = math.sqrt(
-                math.pow(self.x + self.dx - other.x - other.dx, 2) + math.pow(self.y + self.dy - other.y - other.dy, 2))
+        dist_now = math.sqrt(math.pow(self.x-other.x,2)+math.pow(self.y-other.y,2))
+        dist_next = math.sqrt(math.pow(self.x+self.dx-other.x-other.dx,2)+math.pow(self.y+self.dy-other.y-other.dy,2))
+        minDist = (self.diameter + other.diameter) / 2
 
-            if dist_now < self.diameter and dist_next < dist_now:
-                numerator = (self.dx - other.dx) * (self.x - other.x) + (self.dy - other.dy) * (self.y - other.y)
-                denominator = math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2)
-                temp1dx = self.dx - numerator / denominator * (self.x - other.x)
-                temp1dy = self.dy - numerator / denominator * (self.y - other.y)
-                temp2dx = other.dx - numerator / denominator * (other.x - self.x)
-                temp2dy = other.dy - numerator / denominator * (other.y - self.y)
+        if not(self is other) and dist_now < minDist and dist_next < dist_now:
+            numerator = (self.dx - other.dx) * (self.x - other.x) + (self.dy - other.dy) * (self.y - other.y)
+            denominator = math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2)
+            temp1dx = self.dx - numerator / denominator * (self.x - other.x)
+            temp1dy = self.dy - numerator / denominator * (self.y - other.y)
+            temp2dx = other.dx - numerator / denominator * (other.x - self.x)
+            temp2dy = other.dy - numerator / denominator * (other.y - self.y)
 
-                self.dx = temp1dx
-                self.dy = temp1dy
-                other.dx = temp2dx
-                other.dy = temp2dy
+            self.dx = temp1dx
+            self.dy = temp1dy
+            other.dx = temp2dx
+            other.dy = temp2dy
 
 
 class Box:
-
 
     def __init__(self, x, y, h, w, max_x, max_y):
         self.x = x
